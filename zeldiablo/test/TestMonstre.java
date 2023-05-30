aca33e3f744ff7fee876d3d40e50142bdaffe31c
ROTH Tristan <tristanroth00@gmail.com>
2023-05-30 17:13:55 +0200

    ajout de test et modif des fichier laby.txt

--- zeldiablo/test/TestMonstre.java
+++ zeldiablo/test/TestMonstre.java
@@ -1,11 +1,29 @@
+import gameLaby.laby.Entite;
 import gameLaby.laby.Labyrinthe;
+import gameLaby.laby.Monstre;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class TestMonstre {
+    /**
+     * Test si un monstre possède bien une position après la création d'un labyrinthe
+     * @throws IOException
+     */
     @Test
     public void testPosMonstre() throws IOException {
         Labyrinthe laby = new Labyrinthe("laby0.txt");
-        laby.getEntites();
+        ArrayList<Entite> listeEntite = laby.getEntites();
+        assertEquals(5, listeEntite.get(0).getX());
+        assertEquals(2, listeEntite.get(0).getY());
     }
+
+    @Test
+    public void testCaraMonstre(){
+        Labyrinthe laby = new Labyrinthe("laby0.txt");
+        assertEquals('M',laby.MONSTRE);
+    }
+
+
 }
