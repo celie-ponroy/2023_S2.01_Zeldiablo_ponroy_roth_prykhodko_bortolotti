e5cbd6d6b9d3a4f109322acbfa0ba0b6332d06f8
Célie Ponroy <celie.ponroy@laposte.net>
2023-05-31 14:51:43 +0200

    ajout class escalier

---
+++ zeldiablo/src/gameLaby/laby/Escalier.java
@@ -0,0 +1,17 @@
+package gameLaby.laby;
+
+public class Escalier extends Entite{
+        public boolean montant;//si l'escalier monte(true) ou descends(false)
+    Escalier(int x, int y, boolean estmontant){
+        super(x,y,false);
+        this.montant = estmontant;
+    }
+
+    @Override
+    public void deplacer(int[] suiv) {
+        //l'escalier ne se déplace pas
+    }
+    public void changerEtage(){
+        //appel de changerLabyCourant de LabyJeu
+    }
+}
