1afc362e947c0caa43a5b2a89cf431757e2314d1
Yehor Prykhodko <350zegor@gmail.com>
2023-06-02 19:23:27 +0300

    merge comm

--- zeldiablo/src/gameLaby/laby/LabyDessin.java
+++ zeldiablo/src/gameLaby/laby/LabyDessin.java
@@ -53,27 +53,9 @@ public class LabyDessin implements DessinJeu {
         //desssin monstre
 
         for(int i = 0; i<labyrinthe.comb.size(); i++) {
-<<<<<<< HEAD
-            if (labyrinthe.comb.get(i) instanceof Monstre){
-                gc.setFill(Color.PURPLE);
-                //Image image = new Image("images/enemies-2.png");
-                //gc.drawImage(image, labyrinthe.comb.get(i).getX() * 50, labyrinthe.comb.get(i).getY() * 50, 50, 50);
-            }
-
-        }
-        //dessin escalier
-
-        for(int i = 0; i<labyrinthe.entiteInteractives.size(); i++) {
-            if (labyrinthe.entiteInteractives.get(i) instanceof Escalier){
-                gc.setFill(Color.BROWN);
-                gc.fillRect(labyrinthe.entiteInteractives.get(i).getX() * 50, labyrinthe.entiteInteractives.get(i).getY() * 50, 50, 50);
-            }
-
-=======
                 Image imgCombatant = new Image(labyrinthe.comb.get(i).getImage());
                 labyrinthe.comb.get(i).drawComb(gc, imgCombatant, labyrinthe);
-//                gc.drawImage(imgCombatant, labyrinthe.comb.get(i).getX() * 50, labyrinthe.comb.get(i).getY() * 50, 50, 50);
->>>>>>> fb253088a09ca11c9351fa1ad2dfe6a9a4cca227
+
         }
     }
 
