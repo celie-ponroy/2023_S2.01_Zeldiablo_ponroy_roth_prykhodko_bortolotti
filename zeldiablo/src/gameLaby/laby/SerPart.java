41028b91b281faef9e65b4aa11f62e0faaf64097
Yehor Prykhodko <350zegor@gmail.com>
2023-06-02 17:34:08 +0300

    serpent fix

--- zeldiablo/src/gameLaby/laby/SerPart.java
+++ zeldiablo/src/gameLaby/laby/SerPart.java
@@ -1,42 +1,22 @@
 package gameLaby.laby;
 
-public class SerPart {
-    int x, y;
-
+public class SerPart extends Entite {
     public SerPart(int xPos, int yPos) {
-        x = xPos;
-        y = yPos;
+        super(xPos, yPos, true);
     }
-
 
 
     public boolean etrePresent(int dx, int dy) {
         boolean res = false;
 
-        if ( dx ==x && dy == y )
+        if ( dx ==getX() && dy == getY() )
         {
             res = true;
         }
 
         return res;
     }
-
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
+    
 
     public String getImage() {
         return "/corps_serp.png";
