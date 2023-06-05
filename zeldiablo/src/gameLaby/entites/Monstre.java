0c725fc54bd4da4ec18ddf2a63fcf58d7bff4877
Yehor Prykhodko <350zegor@gmail.com>
2023-06-05 11:48:22 +0300

    javadoc

--- zeldiablo/src/gameLaby/entites/Monstre.java
+++ zeldiablo/src/gameLaby/entites/Monstre.java
@@ -2,27 +2,60 @@ package gameLaby.entites;
 
 import gameLaby.laby.*;
 
+/**
+ * Classe Monstre qui hérite de la classe Combattant.
+ * Cette classe représente un monstre dans le labyrinthe.
+ */
 public class Monstre extends Combattant {
 
+    /**
+     * Constructeur du monstre.
+     *
+     * @param vie la quantité de vie du monstre.
+     * @param x position du monstre selon l'axe des abscisses.
+     * @param y position du monstre selon l'axe des ordonnées.
+     * @param col paramètre indiquant si le monstre est solide (true) ou non (false).
+     */
     public Monstre(int vie, int x, int y, boolean col) {
         super(vie, x, y, col);
     }
 
+    /**
+     * Déplace le monstre vers une nouvelle position.
+     *
+     * @param suiv Le tableau contenant les nouvelles coordonnées du monstre.
+     */
     @Override
     public void deplacer(int[] suiv) {
         setX(suiv[0]);
         setY(suiv[1]);
     }
 
+    /**
+     * Attaque un autre combattant.
+     *
+     * @param c Le combattant à attaquer.
+     */
     public void attaquer(Combattant c) {
         c.etreAttaque(Labyrinthe.ATTAQUE_MONSTRE);
     }
+
+    /**
+     * Gère l'attaque reçue par le monstre.
+     *
+     * @param force La force de l'attaque reçue.
+     */
 
     public void etreAttaque(int force) {
         int pvRestant = this.getPv() - force;
         this.setPv(pvRestant);
     }
 
+    /**
+     * Méthode à surcharger dans les classes dérivées pour renvoyer le chemin de l'image du monstre.
+     *
+     * @return null par défaut. À surcharger dans les classes dérivées.
+     */
     @Override
     public String getImage() {
         return null;
--- zeldiablo/src/gameLaby/entites/Perso.java
+++ zeldiablo/src/gameLaby/entites/Perso.java
@@ -3,16 +3,17 @@ package gameLaby.entites;
 import gameLaby.laby.*;
 
 /**
- * gere un personnage situe en x,y
+ * Classe Perso qui hérite de la classe Combattant. Cette classe représente un personnage situé en (x, y) dans le labyrinthe.
  */
 public class Perso extends Combattant {
 
 
     /**
-     * constructeur
+     * Constructeur du personnage.
      *
-     * @param dx position selon x
-     * @param dy position selon y
+     * @param dx Position selon l'abscisse.
+     * @param dy Position selon l'ordonnée.
+     * @param col Booléen indiquant si le personnage peut être en collision.
      */
     public Perso(int dx, int dy, boolean col) {
         super(Labyrinthe.VIE_PERSO, dx, dy, col);
@@ -20,31 +21,52 @@ public class Perso extends Combattant {
 
 
     /**
-     * permet de savoir si le personnage est en x,y
+     * Permet de savoir si le personnage est à une certaine position (dx, dy).
      *
-     * @param dx position testee
-     * @param dy position testee
-     * @return true si le personnage est bien en (dx,dy)
+     * @param dx Position selon l'abscisse à tester.
+     * @param dy Position selon l'ordonnée à tester.
+     * @return true si le personnage est bien à la position (dx, dy).
      */
     public boolean etrePresent(int dx, int dy) {
         return (this.getX() == dx && this.getY() == dy);
     }
 
+    /**
+     * Déplace le personnage vers une nouvelle position.
+     *
+     * @param suiv Le tableau contenant les nouvelles coordonnées du personnage.
+     */
     @Override
     public void deplacer(int[] suiv) {
         setX(suiv[0]);
         setY(suiv[1]);
     }
 
+    /**
+     * Attaque un autre combattant.
+     *
+     * @param c Le combattant à attaquer.
+     */
     public void attaquer(Combattant c) {
         c.etreAttaque(Labyrinthe.ATTAQUE_PERSO);
     }
 
+    /**
+     * Gère l'attaque reçue par le combattant.
+     *
+     * @param force La force de l'attaque reçue.
+     */
     public void etreAttaque(int force) {
         int pvRestant = this.getPv() - force;
         this.setPv(pvRestant);
     }
 
+
+    /**
+     * Renvoie le chemin d'accès à l'image du personnage.
+     *
+     * @return Le chemin d'accès à l'image du personnage.
+     */
     @Override
     public String getImage() {
         return "link-2.png";
